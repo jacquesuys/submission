a062c2b56889b8dc457e7ff61983555523739a27
Jacques Uys <jaxuys@gmail.com>
2016-05-13 13:02:35 +0200

    very naive test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,18 +5,18 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('fundlistApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, $scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+    $scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: $scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should not have an empty object on the fund', function () {
+    expect($scope.fund).not.toBeNull();
   });
+
 });
